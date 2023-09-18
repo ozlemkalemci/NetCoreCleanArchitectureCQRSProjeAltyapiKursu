fe8dc47a7ed7732e8d9aa6af93db1f4c9a5ba0e5
Özlem Kalemci <ozlmklmc@gmail.com>
2023-09-18 03:27:34 +0300

    create

---
+++ nArchitectureDemo/src/corePackages/Core.Packages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -0,0 +1,70 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories
+{
+    // IAsyncRepository adlı bir arabirim (interface) tanımlanıyor.
+    public interface IAsyncRepository<TEntity, TEntityId> : IQueryable<TEntity>
+        where TEntity : Entity<TEntityId>
+    {
+        // Belirli bir koşula göre bir varlık almak için kullanılır.
+        Task<TEntity?> GetAsync(
+            Expression<Func<TEntity, bool>> predicate, // Koşulu ifade eden bir fonksiyon
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için
+            bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+            bool enableTracking = true, // İzleme (tracking) özelliğini etkinleştirmek için
+            CancellationToken cancellationToken = default // İşlemi iptal etmek için bir işaretçi
+        );
+
+        // Birden çok varlığı liste halinde almak için kullanılır.
+        Task<Paginate<TEntity>> GetListAsync(
+            Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, // Verilerin nasıl sıralanacağını belirlemek için (isteğe bağlı)
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için (isteğe bağlı)
+            int index = 0, // Sayfa numarası
+            int size = 10, // Sayfa boyutu
+            bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+            bool enableTracking = true, // İzleme (tracking) özelliğini etkinleştirmek için
+            CancellationToken cancellationToken = default // İşlemi iptal etmek için bir işaretçi
+        );
+
+        // Dinamik bir sorguya göre bir varlık listesi almak için kullanılır.
+        Task<Paginate<TEntity>> GetListByDynamicAsync(
+            DynamicQuery dynamic, // Dinamik sorgu nesnesi
+            Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için (isteğe bağlı)
+            int index = 0, // Sayfa numarası
+            int size = 10, // Sayfa boyutu
+            bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+            bool enableTracking = true, // İzleme (tracking) özelliğini etkinleştirmek için
+            CancellationToken cancellationToken = default // İşlemi iptal etmek için bir işaretçi
+        );
+
+        // Veri var mı yok mu sorgusu için kullanılır.
+        Task<bool> AnyAsync(
+            Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+            bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+            bool enableTracking = true, // İzleme (tracking) özelliğini etkinleştirmek için
+            CancellationToken cancellationToken = default // İşlemi iptal etmek için bir işaretçi
+        );
+
+        // Bu metot, veritabanına yeni bir varlık (TEntity) eklemek için kullanılır.
+        Task<TEntity> AddAsync(TEntity entity);
+
+        // Bu metot, birden çok varlığı (ICollection<TEntity>) veritabanına eklemek için kullanılır.
+        Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entity);
+
+        // Bu metot, varlık (TEntity) üzerinde yapılan değişiklikleri veritabanına kaydetmek için kullanılır.
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        // Bu metot, birden çok varlığın (ICollection<TEntity>) üzerinde yapılan değişiklikleri veritabanına kaydetmek için kullanılır.
+        Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entity);
+
+        // Bu metot, belirtilen varlığı (TEntity) veritabanından silmek için kullanılır.
+        Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false);
+
+        // Bu metot, birden çok varlığın (ICollection<TEntity>) veritabanından silmek için kullanılır.
+        Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entity, bool permanent = false);
+    }
+}
---
+++ nArchitectureDemo/src/corePackages/Core.Packages/Core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,60 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public interface IRepository<TEntity, TEntityId> : IQueryable<TEntity>
+        where TEntity : Entity<TEntityId>
+{
+    TEntity? Get(
+        Expression<Func<TEntity, bool>> predicate, // Koşulu ifade eden bir fonksiyon
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için
+        bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+        bool enableTracking = true // İzleme (tracking) özelliğini etkinleştirmek için
+   );
+
+    Paginate<TEntity> GetList(
+        Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, // Verilerin nasıl sıralanacağını belirlemek için (isteğe bağlı)
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için (isteğe bağlı)
+        int index = 0, // Sayfa numarası
+        int size = 10, // Sayfa boyutu
+        bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+        bool enableTracking = true // İzleme (tracking) özelliğini etkinleştirmek için
+     );
+
+    Paginate<TEntity> GetListByDynamic(
+        DynamicQuery dynamic, // Dinamik sorgu nesnesi
+        Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, // İlişkili varlıkları içe almak için (isteğe bağlı)
+        int index = 0, // Sayfa numarası
+        int size = 10, // Sayfa boyutu
+        bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+        bool enableTracking = true // İzleme (tracking) özelliğini etkinleştirmek için
+     );
+
+    bool Any(
+        Expression<Func<TEntity, bool>>? predicate = null, // Koşulu belirlemek için (isteğe bağlı)
+        bool withDeleted = false, // Silinmiş varlıkları dahil etmek için
+        bool enableTracking = true // İzleme (tracking) özelliğini etkinleştirmek için
+       );
+
+    TEntity Add(TEntity entity);
+
+    ICollection<TEntity> AddRange(ICollection<TEntity> entities);
+
+    TEntity Update(TEntity entity);
+
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entities);
+
+    TEntity Delete(TEntity entity, bool permanent = false);
+
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
+
+}
